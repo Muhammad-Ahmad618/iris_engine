e418ca9be3d8a1c85fbc891096c0249d0b66d285
M-Ahmad676 <125358039+M-Ahmad676@users.noreply.github.com>
2024-12-16 00:24:58 +0500

    update 1.2

--- src/Components/HorizontalGameCard.jsx
+++ src/Components/HorizontalGameCard.jsx
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from 'react';
+import { FaStar, FaStarHalfAlt  } from "react-icons/fa";
+import { IoMdAddCircle } from "react-icons/io";
 
-export default function HorizontalGameCard({ FetchGames }) {
+export default function HorizontalGameCard({title, FetchDetailedGames }) {
     const [game, setGames] = useState([]);
 
     useEffect(()=> {
        
       const FetchData = async() => {
-      const responseData = await FetchGames('3',1,'2023-01-01,2023-11-30')
+      const responseData = await FetchDetailedGames('4',1,'2020-01-01,2024-11-30','Elden Ring Shadow of the Erd Tree')
     
         // Ensure that the response is an array or fallback to an empty array
         if (Array.isArray(responseData)) {
@@ -18,22 +20,39 @@ export default function HorizontalGameCard({ FetchGames }) {
         
       FetchData()
   
-    },[FetchGames])
+    },[FetchDetailedGames])
 
     if (game.length === 0) {
         return <div className='text-center text-[1.3rem] text-white'>No games found.</div>;
       }
   
   return (
-    <div className=' w-full h-[25rem]'>
-      <div className='max-w-[35rem]'>
-          <img src={game[0].background_image} alt={game[0].name} className='rounded-md w-full' />
+    <div className='group'>
+    <h3 className='text-[1.8rem] text-white font-medium pb-8'>{title}</h3>
+    <div className='flex gap-x-4 w-full bg-[#12121279] rounded-md text-white'>
+      <div className='max-w-[30rem] cursor-pointer relative'>
+      <IoMdAddCircle className='text-[1.8rem] absolute top-2 right-2 z-10 hidden group-hover:block hover:text-[#d946f0]'/>
+          <img src={game[0].background_image} alt={game[0].name} className='rounded-md w-full group-hover:opacity-80 transition-all duration-300' />
       </div>
-      <div>
-        <h1>{game[0].name}</h1>
-        <p>{game[0].description_raw}</p>
-        <p>{game[0].rating}</p>
+      <div className='p-4 space-y-2'>
+        <h1 className='text-[2rem] font-medium cursor-pointer'
+        style={{ textShadow: '1px 1px 2px black' }}>{game[0].name}</h1>
+        <p className='text-sm text-[#35f462] font-medium'>Release Date : {game[0].released}</p>
+        <p className='text-xs text-gray-200 leading-relaxed'>{game[0].description}</p>
+        
+        <div className='flex items-center gap-x-2 text-sm'>
+          <ul className='flex text-yellow-400 gap-x-1'>
+            <li><FaStar/></li>
+            <li><FaStar/></li>
+            <li><FaStar/></li>
+            <li><FaStar/></li>
+            <li><FaStarHalfAlt/></li>
+          </ul>
+        <p className='font-medium text-yellow-400'>({game[0].rating})</p>
+        </div>
+        <p className=''>{game[0].publishers} | {game[0].developers}</p>
       </div>
     </div>
+    </div> 
   );
 }
--- src/Screens/Store.jsx
+++ src/Screens/Store.jsx
@@ -3,25 +3,24 @@ import NavBar from '../Components/StoreNavBar'
 import CoverSlider from '../Components/StoreCoverSlider'
 import NewGamesSlider from '../Components/NewGamesSlider'
 import Categories from '../Components/SlickSlider'
-import GameCover from '../Components/GameCover'
+import GameCoverCards from '../Components/GameCoverCards'
 import axios from 'axios'
 import HorizontalGameCard from '../Components/HorizontalGameCard'
 
 export default function Store() {
 
+  const API_KEY =  'a82e5a54c5794044a40b36a465e6c265';
+
   const fetchGameData = async(genre,numbers,dates,specifics) => {
-    
-    const API_KEY =  'a82e5a54c5794044a40b36a465e6c265';
+  
     let API_url = `https://api.rawg.io/api/games?key=${API_KEY}&dates=${dates}&platforms=4&genres=${genre}&ordering=-metacritics&page_size=${numbers}`;
     
     if(specifics){
       API_url +=`&search=${specifics}`
     }
     try {
-      // Make the API call
       const response = await axios.get(API_url);
       
-      // Check if 'results' exists in the response
       if (response.data && response.data.results) {
         return response.data.results;  // Return the results if available
       } else {
@@ -34,6 +33,41 @@ export default function Store() {
     }
   }
 
+  const fetch_Detailed_Game_Date = async (genre,numbers,dates,specifics) => {
+
+    let API_url = `https://api.rawg.io/api/games?key=${API_KEY}&dates=${dates}&platforms=4&genres=${genre}&ordering=-metacritics&page_size=${numbers}`;
+    
+    if(specifics){
+      API_url +=`&search=${specifics}`
+    }
+    
+    try{
+    const response = await axios.get(API_url)
+    const gameList = response.data.results;
+
+    const game_With_Details = await Promise.all(
+      gameList.map(async(game) => {
+        const gameDetails = await axios.get(
+          `https://api.rawg.io/api/games/${game.id}?key=${API_KEY}`
+        );
+        return {
+          ...game,
+          description: gameDetails.data.description_raw,
+          publishers: gameDetails.data.publishers.map(pub => pub.name).join(","),
+          tags: gameDetails.data.tags.map(tag => tag.name).join(", "),
+          developers:gameDetails.data.developers.map(dev => dev.name).join(",")
+        }
+      })
+    )
+    return game_With_Details
+  }
+  catch(error){
+    console.log("Error while Fetching data", error)
+    return error
+  }
+  
+  }
+
   return (
     <div className='min-h-screen bg-custom-gradient'>
       <NavBar/>
@@ -42,8 +76,9 @@ export default function Store() {
       <div className='px-32 pt-28 space-y-16'>
       <NewGamesSlider/>      
       <Categories/>
-      <GameCover title="Top Speed" FetchGames={fetchGameData} />
-      <HorizontalGameCard FetchGames={fetchGameData}/>
+      <GameCoverCards title="Top Speed" FetchGames={fetchGameData} />
+      <HorizontalGameCard  title="Game of the Year" FetchDetailedGames={fetch_Detailed_Game_Date}/>
+      <GameCoverCards title="Trending" FetchGames={fetchGameData}/>
       </div>
       </div>
     </div>
